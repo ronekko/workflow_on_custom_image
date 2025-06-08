f375c7394fc36ab785580a4a0c3bff8f76ecc587
ronekko <ronekko@gmail.com>
2025-06-08 20:59:28 +0900

    環境構築スクリプトを作成する

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,26 @@
+apt-get update
+apt-get install -y \
+    curl \
+    git \
+    build-essential \
+    cmake \
+    lld \
+    ccache \
+    libboost-dev \
+    libfmt-dev \
+    libspdlog-dev \
+    libeigen3-dev \
+    libopencv-dev \
+    libssl-dev \
+    libomp-dev \
+    libdw-dev \
+    doxygen \
+    graphviz \
+    libboost-program-options-dev \
+    libboost-system-dev \
+    libboost-thread-dev \
+    software-properties-common \
+    python3-pip \
+    python3-psutil \
+    vlc \
+    clang-format
